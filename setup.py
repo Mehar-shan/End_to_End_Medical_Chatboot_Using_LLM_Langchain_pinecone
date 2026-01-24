ea0f823c358500d5b3b4aaa8511e417926b329a8
Mehar-shan <meharalimeharali890@gmail.com>
2026-01-24 00:01:47 -0800

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="0.0.0",
+    author="Mehar Shan Ali",
+    author_email="smartebussinesstechnologies576@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
